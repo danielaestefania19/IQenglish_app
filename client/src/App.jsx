2ba02472b5eed1746d85373c9427c96870fddb67
danielaestefania19 <danielacastellano1000@gmail.com>
2024-04-21 21:44:39 -0600

    Changes Front End

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
-import { Button } from "flowbite-react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./landing/Navbar"
-import Component from "./components/Prospects"
-function App() {
+import Body from "./landing/Body"
+//import Form from "./landing/Form"
+import Footer from "./landing/Footer"
+// import Carousel from "./landing/Carousel"
+import Card from "./landing/Card" 
+import Blog from "./landing/Blog"
+import Teachers from "./landing/Teachers"
 
+
+
+
+
+
+function App() {
   return (
-    <Component />
+    <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <Body />
+            <Blog />
+            <Card/>
+            <Footer />
+          </>
+        }
+      />
+      <Route path="/teachers" element={<Teachers />} />
+    </Routes>
+    </Router>
   )
 }
+
 
 export default App
--- client/src/landing/Navbar.jsx
+++ client/src/landing/Navbar.jsx
@@ -1,23 +1,67 @@
-const Navbar = () => {
-    return (
-      <nav className="bg-gradient-to-b from-[#0c0e15] to-[#323335]">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a className="flex items-center space-x-3 rtl:space-x-reverse">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white ">Creativas</span>
-          </a>
-          <h1 to="/" className="p-4 text-white">Home</h1>
-          <h1 className="p-4 text-white">
-              <a> Docs </a>
-          </h1>
-        
-              <button className="inline-flex items-center justify-center rounded-xl bg-[#1C00FF] borderpx-3 py-2 text-sm font-semibold text-white  shadow-sm transition-all duration-150">
-                  <div className="flex items-center">Logout </div>
+import logo1 from "../assets/logo1.jpeg";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <header className="absolute left-0 top-0 z-50 w-full">
+      <div className="container mx-auto">
+        <div className="relative -mx-4 flex items-center justify-between">
+          <div className="w-60 max-w-full px-4">
+            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src={logo1} alt="Logo" className="h-10 w-auto" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">IQEnglish</span>
+            </a>
+          </div>
+          <div className="flex w-full items-center justify-between px-4">
+            <div>
+              <button
+                onClick={() => setNavbarOpen(!navbarOpen)}
+                className={`absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden ${navbarOpen ? 'navbarTogglerActive' : ''}`}
+                id="navbarToggler"
+              >
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
               </button>
-  
+              <nav
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow transition-all dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none xl:ml-11 ${!navbarOpen ? 'hidden' : ''}`}
+                id="navbarCollapse"
+              >
+                <ul className="block lg:flex">
+                  <li>
+                  <Link to="/" href="javascript:void(0)" className="flex py-2 text-base font-medium text-dark hover:text-primary dark:text-white lg:ml-10 lg:inline-flex">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                  <Link to="/teachers" className="flex py-2 text-base font-medium text-dark hover:text-primary dark:text-white lg:ml-10 lg:inline-flex">
+                      Equipo
+                    </Link>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)" className="flex py-2 text-base font-medium text-dark hover:text-primary dark:text-white lg:ml-10 lg:inline-flex">
+                      Metodo
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
+              <a href="javascript:void(0)" className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white">
+                Login
+              </a>
+              <a href="javascript:void(0)" className="rounded-md bg-primary px-7 py-3 text-base font-medium text-white hover:bg-blue-dark">
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-  </nav>
-    );
-  };
-  
-  export default Navbar;
-  
+    </header>
+  );
+}
+
+export default Navbar
