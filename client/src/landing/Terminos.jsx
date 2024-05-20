30849bb7e6fe69553174e513a0b8275da937b7d5
danielaestefania19 <danielacastellano1000@gmail.com>
2024-05-20 14:58:38 -0600

    Changes

---
+++ client/src/landing/Terminos.jsx
@@ -0,0 +1,64 @@
+const Terminos = () => {
+    return (
+        <div className="max-w-3xl mx-auto mt-8 p-4 bg-white shadow-md rounded-lg">
+        <h2 className="text-2xl font-bold mb-4">Términos y Condiciones</h2>
+        
+        <p className="mb-4">
+          El presente aviso de privacidad rige el tratamiento de los datos personales por parte de IQ ENGLISH S.A. DE C.V., empresa que opera la Enseñanza del Idioma Inglés bajo la marca que para efectos del presente aviso se denominará “IQ ENGLISH”, de conformidad con los términos previstos por la Ley Federal de Protección de Datos Personales en Posesión de los Particulares (la Ley o la LFPDPPP) y por las disposiciones reglamentarias y/o administrativas derivadas o relacionadas con la Ley.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">INFORMACIÓN QUE SE RECOLECTA Y MECANISMO DE RECOLECCIÓN</h3>
+        
+        <p className="mb-4">
+          IQ ENGLISH, S.A. DE C.V. trata sus datos personales, es decir, aquella información que puede identificarle razonablemente y que puede constar en diversos documentos, sean impresos o en mensajes de datos. Los datos personales que se recolectan y/o tratan incluyen entre otros: su nombre y apellidos, domicilio (particular o de trabajo o fiscal), dirección de correo electrónico (personal o del trabajo) número telefónico fijo o celular (particular o del trabajo) número de tarjeta de crédito o débito, datos fiscales, información profesional u otros de naturaleza análoga que permitan a IQ ENGLISH, S.A. DE C.V. operar y cumplir con sus obligaciones comerciales, administrativas y laborales. Los datos personales se recolectan mediante: I) el uso de correos electrónicos; II) comunicación telefónica con nuestro personal autorizado; III) el suministro voluntario de información y datos personales en entrevistas con personal autorizado de IQ ENGLISH, IV) llenado de formatos y/o impresos o digitales, y V) la utilización de fuentes de acceso público y de otras fuentes disponibles en el mercado.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">OBJETIVOS DEL TRATAMIENTO DE LA INFORMACIÓN</h3>
+        
+        <p className="mb-4">
+          Confirmar la identidad del cliente y/o de su representante legal. Cumplir con los requerimientos legales que le son aplicables. Proveer información sobre: condiciones, políticas y disposiciones para la contratación y el uso de los servicios, medios de pago, modalidades de los servicios. Envío de material promocional de IQ ENGLISH. Envío de facturas Cualquier otra actividad de naturaleza análoga a las descritas en los incisos anteriores.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">EN EL ÁMBITO LABORAL LOS OBJETIVOS DEL TRATAMIENTO DE LOS DATOS PERSONALES SON</h3>
+        
+        <p className="mb-4">
+          1.- Selección, reclutamiento y contratación de personal. 2.- Gestión de prestaciones al personal. 3.- Pago de nómina. 4.- Pago de retribuciones, impuestos y aportaciones del personal. 5.- Pago de contribuciones y obligaciones patronales. 6.- Gestión de capacitación. 7.- Control interno sobre el personal y recursos asignados. 8.- Cualquier otra actividad de naturaleza análoga a las descritas en los incisos previamente citados.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">TRANSFERENCIA DE DATOS</h3>
+        
+        <p className="mb-4">
+          IQ ENGLISH hace de su conocimiento que, con el objeto de cumplir con sus obligaciones comerciales, incluyendo la prestación de servicios a sus clientes, así como para cumplir con sus obligaciones laborales ha celebrado o celebrará diversos contratos con proveedores ubicados en territorio nacional para que le suministren distintos servicios que le permitan llevar a cabo sus actividades en México, en el entendido de que dichos proveedores están obligados a mantener la confidencialidad de los datos personales suministrados y a observar los Avisos de Privacidad de IQ ENGLISH; así mismo, podrá transmitir los datos personales a las empresas dedicadas al marketing de IQ ENGLISH bajo el principio de confidencialidad, observando el presente aviso y sujetos a las mismas políticas en materia de protección de datos.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">MEDIDAS PARA PROTEGER LOS DATOS PERSONALES</h3>
+        
+        <p className="mb-4">
+          Los datos personales tratados por IQ ENGLISH se encuentran protegidos por medidas de seguridad administrativas, técnicas y físicas adecuadas contra el daño, pérdida, alteración, destrucción o uso, acceso o tratamiento no autorizado, de conformidad con lo dispuesto en la LFPDPPP y en la regulación administrativa que derive de la misma. Todos los datos personales almacenados en formato electrónico se mantienen en bancos de datos confidenciales y seguros.
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">MEDIOS PARA HACER VALER LOS DERECHOS ARCO</h3>
+        
+        <p className="mb-4">
+          Usted tiene en todo momento el derecho a conocer qué datos personales tenemos de usted, la finalidad de su utilización y las condiciones de uso que le damos (acceso), así mismo, es su derecho solicitar la corrección (rectificación), también tiene derecho a que su información se elimine de nuestros registros o bases de datos cuando considere que la misma no está siendo utilizada adecuadamente (cancelación) o la facultad para oponerse al uso de sus datos personales para fines específicos (oposición). A estos derechos se les conoce como derechos ARCO.
+        </p>
+        
+        <p className="mb-4">
+          Para ejercer los Derechos ARCO, el Titular o su representante puede presentar ante el departamento de Atención a Clientes de IQ ENGLISH una solicitud de acceso, rectificación, cancelación u oposición al tratamiento de datos a través de los siguientes medios:
+          Teléfono: 246 32 89 286. Email: administracion@iqenglish.mx
+        </p>
+        
+        <p className="mb-4">
+          Si usted desea dejar de recibir mensajes promocionales de nuestra parte puede solicitarlo al: Teléfono: 246 32 89 286. Email: administracion@iqenglish.mx
+        </p>
+        
+        <h3 className="text-lg font-bold mb-2">CAMBIOS DE AVISO DE PRIVACIDAD</h3>
+        
+        <p>
+          Para cualquier consulta que tenga en torno a la protección de sus datos personales, sírvase contactar al Departamento de Atención al Clientes: Calle San Pablo No. 4 interior 2, Ocotlán, Tlaxcala, C.P. 90010
+        </p>
+      </div>
+    );
+  }
+
+  export default Terminos
--- client/src/nosotros/Nosotros.jsx
+++ client/src/nosotros/Nosotros.jsx
@@ -1,38 +1,43 @@
 import Nosotros1 from "../assets/Nosotros1.png";
 import Nosotros2 from "../assets/Nosotros2.png";
-
+import Nosotros3 from "../assets/Nosotros3.jpg"
 
 const Nosotros = () => {
     return (
 
         <div className="flex flex-col items-center justify-center h-full mt-16">
-              <div className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-7xl p-0 mb-8">
-                <img src={Nosotros1} className="object-cover w-full h-128 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-s-lg" /> 
+            <div className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-7xl p-0 mb-8">
+                <img src={Nosotros1} className="object-cover w-full h-128 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-s-lg" />
                 <div className="flex flex-col justify-between p-2 leading-normal">
                     <h5 className="mb-2 text-5xl font-bold tracking-tight text-black">¿Quiénes somos?</h5>
                     <p className="mb-3 font-normal text-gray-900 text-[18px] ">
-                    IQ English es el resultado de la pasión por compartir conocimientos y por hacer de la educación algo accesible. Somos una institución con un sistema de enseñanza innovador, eficiente, personalizado y garantizado que forma líderes bilingües que potencian su vida personal y profesional al superar positivamente cualquier comunicación en inglés. Desde que comenzamos a operar, hemos tenido la oportunidad de ayudar a que muchas personas adquieran habilidades y nuevas técnicas para desarrollarse mejor.</p>
+                        IQ English es el resultado de la pasión por compartir conocimientos y por hacer de la educación algo accesible. Somos una institución con un sistema de enseñanza innovador, eficiente, personalizado y garantizado que forma líderes bilingües que potencian su vida personal y profesional al superar positivamente cualquier comunicación en inglés. Desde que comenzamos a operar, hemos tenido la oportunidad de ayudar a que muchas personas adquieran habilidades y nuevas técnicas para desarrollarse mejor.</p>
                 </div>
             </div>
             <div className="flex flex-col items-center justify-center h-full">
                 <div className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row-reverse md:max-w-7xl p-0 mb-8">
-                     <img src={Nosotros2} className="object-cover w-full h-128 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-e-lg" /> 
+                    <img src={Nosotros2} className="object-cover w-full h-128 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-e-lg" />
                     <div className="flex flex-col justify-between p-4 leading-normal">
-                        <h5 className="mb-2 text-5xl font-bold tracking-tight text-black">Nuestra Visión</h5>
-                        <p className="mb-3 font-normal text-gray-900 text-[18px]">Trascender como la mejor institución en Latinoamérica, formadora de líderes bilingües capaces de potenciar su vida personal y profesional al superar toda comunicación en inglés.</p>
+                        <h5 className="mb-2 text-5xl font-bold tracking-tight text-black">Nuestra Misión</h5>
+                        <p className="mb-3 font-normal text-gray-900 text-[18px]">Ser un equipo de profesionales, expertos en la enseñanza del idioma inglés, comprometidos a formar un México bilingüe.
+
+                            Nuestros orgullosos graduados nos recomiendan por brindar una experiencia única de aprendizaje y por nuestra metodología innovadora, personalizada y eficaz; posicionándonos como la marca líder en Latinoamérica.</p>
                     </div>
                 </div>
             </div>
             <div className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-7xl p-0 mb-8">
-                 <img src={Nosotros2} className="object-cover w-full h-128 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-s-lg" /> 
+                <img src={Nosotros3} className="object-cover w-full h-96 md:w-96 md:h-auto rounded-t-lg md:rounded-none md:rounded-l-lg" />
                 <div className="flex flex-col justify-between p-2 leading-normal">
                     <h5 className="mb-2 text-5xl font-bold tracking-tight text-black">Nuestra Visión</h5>
-                    <p className="mb-3 font-normal text-gray-900 text-[18px] ">
-                    Trascender como la mejor institución en Latinoamérica, formadora de líderes bilingües capaces de potenciar su vida personal y profesional al superar toda comunicación en inglés.</p>
+                    <p className="mb-3 font-normal text-gray-900 text-lg">
+                        Trascender como la mejor institución en Latinoamérica, formadora de líderes bilingües capaces de potenciar su vida personal y profesional al superar toda comunicación en inglés.
+                    </p>
                 </div>
             </div>
 
-            
+
+
+
         </div>
     );
 }
