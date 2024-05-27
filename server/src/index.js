bf445adcb675d3e6af25b8bdb3cfc42539af6317
Alfredo <alfredocastellanoula.14@gmail.com>
2024-05-27 16:22:41 -0600

    Deploy

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,6 @@
-import express from 'express'
-import prospectsRoutes from './routes/prospects.routes.js'
-import advisorsRoutes from './routes/advisors.routes.js'
-import blogsRoutes from './routes/blogs.routes.js'
-import reviewsRoutes from './routes/reviews.routes.js'
+import app from './app.js'
 import { initializeAdmin } from './init.js'
-import dotenv from 'dotenv'
-import cors from "cors";
-const app = express()
 
-
-
-dotenv.config()
-
-
-app.use(cors());
-app.use(express.json())
-app.use('/api', prospectsRoutes)
-app.use('/api', advisorsRoutes)
-app.use('/api', blogsRoutes)
-app.use('/api', reviewsRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'endpoint not found'
-    })
-})
 
 // Inicializar el usuario administrador
 if (process.env.NODE_ENV !== 'test') {
