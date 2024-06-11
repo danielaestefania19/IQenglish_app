9811bf7d644b0d85d0aa0fa5942b96a19241b8d0
Alfredo <alfredocastellanoula.14@gmail.com>
2024-06-11 17:28:15 -0600

    Responsives

--- src/landing/Banner.jsx
+++ src/landing/Banner.jsx
@@ -5,16 +5,18 @@ import "../index.css"; // Archivo CSS para estilos adicionales
 
 const Banner = () => {
   return (
-    <div id="sticky-banner" tabIndex="-1" className="fixed  top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-[#F0F4F9] dark:bg-gray-700 dark:border-gray-600 mb-8">
+    <div id="sticky-banner" tabIndex="-1" className="fixed top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-[#F0F4F9] dark:bg-gray-700 dark:border-gray-600 mb-8">
       <div className="flex items-center mx-auto">
         <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400 banner-text">
           <img src={cohete} alt="Rocket icon" className="w-6 h-6 mr-3" />
           <span className="animated-text">
-            <span>Promocion</span> <span className="sale">Limitada</span>  60% OFF para que seas bilingüe este 2024
+            <span>Promoción</span> <span className="sale">Limitada</span>  60% OFF para que seas bilingüe este 2024
           </span>
-          <Button color="primary" size='sm' variant='shadow' className="ml-4">
-           Empieza hoy
-          </Button>  
+          <Button color="primary" size="sm" variant='shadow' className="ml-4 flex flex-col -gap-2">
+  <p className="mb-0">Empieza</p>
+  <p className="mt-0">Hoy</p>
+</Button> 
+
         </p>
       </div>
       <div className="flex items-center"></div>
